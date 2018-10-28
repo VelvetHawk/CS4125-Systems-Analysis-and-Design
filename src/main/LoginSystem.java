98a863910467d0f20dde1e27a35139f5a228eb54
conor96 <conormoloney57@gmail.com>
2018-10-28 19:16:23 +0000

    Created LoginSystem.java
    Driver class for the login system

---
+++ src/main/LoginSystem.java
@@ -0,0 +1,10 @@
+public class LoginSystem {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        LoginPage login = new LoginPage();
+    }
+    
+}
