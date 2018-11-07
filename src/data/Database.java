df8de3fbfb21c56a6aa8d98008973e152f8830dd
VelvetHawk <artem.smnv@yahoo.ie>
2018-11-07 00:48:15 +0000

    Added Facade pattern to data package. Added Database enum to differentiate between different types of DB connections

---
+++ src/data/Database.java
@@ -0,0 +1,7 @@
+package data;
+
+public enum Database
+{
+	MYSQL,
+	AWS
+}
---
+++ src/data/DatabaseFacade.java
@@ -0,0 +1,13 @@
+package data;
+
+import java.sql.Connection;
+
+public class DatabaseFacade
+{
+	
+	public static Connection getConnection(Database database)
+	{
+		// TODO: Create implementation
+		return null;
+	}
+}
