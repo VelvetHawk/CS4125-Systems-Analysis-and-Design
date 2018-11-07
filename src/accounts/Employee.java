7c07a0c3341e34927c8b71888ecd0c0e9acf6db9
VelvetHawk <artem.smnv@yahoo.ie>
2018-11-07 00:16:08 +0000

    Moved Employee enum from StaffAccount and made it accessible in the accounts package.

---
+++ src/accounts/Employee.java
@@ -0,0 +1,7 @@
+package accounts;
+
+public enum Employee
+{
+	EMPLOYEE,
+	MANAGER
+}
--- src/accounts/StaffAccount.java
+++ src/accounts/StaffAccount.java
@@ -2,12 +2,6 @@ package accounts;
 
 public class StaffAccount extends Account
 {
-	protected enum Employee
-	{
-		EMPLOYEE,
-		MANAGER
-	}
-	
 	protected int staffNumber;
 	protected Employee employeeType;
 	
