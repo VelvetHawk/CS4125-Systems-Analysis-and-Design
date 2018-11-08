fd323e334896f7a74b5f019a856f976d819b7b07
conor96 <conormoloney57@gmail.com>
2018-11-08 23:10:36 +0000

    Create LoginController
    Controller is responsible for the business logic layer of the log-in process

---
+++ src/main/LoginController.java
@@ -0,0 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+//package com.mvc.controller;
+
+//import com.mvc.model.LoginModel;
+//import com.mvc.view.LoginView;
+
+import java.awt.event.ActionListener;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.*;
+
+/**
+ *
+ * @author conor_000
+ */
+public class LoginController {
+    
+    private LoginView view;
+    private LoginModel model;
+    boolean loginValid = false;
+    
+    public LoginController(LoginView view){
+         this.view = view;
+         model = new LoginModel();
+   }
+    
+   
+    
+   
+   public void checkCredentials(String username, char[] password){
+       
+       
+
+       try {
+         
+           model.loginValidation(username, password);
+           loginValid = model.getLogin();
+           if(loginValid){
+               view.setMessage("Loggin in");
+           }
+           else{
+               System.out.println("invalid");
+               view.setMessage("invalid name or password"); 
+           }
+       } catch (Exception ex) {
+           //Logger.getLogger(LoginPage.class.getName()).log(Level.SEVERE, null, ex);
+       }
+   }
+   
+}
