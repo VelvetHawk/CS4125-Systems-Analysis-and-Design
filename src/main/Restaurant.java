45e773c28f0f865f0cfcc3bf1759c0aebb324ef5
VelvetHawk <artem.smnv@yahoo.ie>
2018-11-13 12:33:04 +0000

    Updated testing code

--- src/main/Restaurant.java
+++ src/main/Restaurant.java
@@ -11,10 +11,28 @@ public class Restaurant
 	{
 		//RegisterLogin rl = new RegisterLogin();
 		
+		
 		// REMOVE LATER, FOR TESTING
 		JFrame frame = new JFrame();
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.add(new CartListPanel("Example Title", '\u20AC', 2));
+		
+		// Setup cart list
+		CartListPanel panel = new CartListPanel("Sample Title", '\u20AC', 2);
+		panel.setAddItemActionListener(event ->
+		{
+			panel.addItem(5.00, "Item name", "MEDIUM");
+			panel.updateSelf();
+			
+		});
+		for (int i = 0; i < 10; i++)
+			panel.addItem(5.00, "Item name", "MEDIUM");
+		panel.updateSelf();
+		frame.add(panel);
+		
+		// Setup choice panel
+//		StaticChoicePanel<String> panel2 = new StaticChoicePanel<>("Select:");
+//		frame.add(panel2);
+		
 		frame.setSize(400, 500);
 		frame.setLocationRelativeTo(null); // Center
 		frame.setVisible(true);
