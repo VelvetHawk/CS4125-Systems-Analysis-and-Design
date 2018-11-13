466a91279bb0e4968b4860c9da11da73c1a99165
VelvetHawk <artem.smnv@yahoo.ie>
2018-11-13 12:31:33 +0000

    Testing code to run CartListPanel in main

--- src/main/Restaurant.java
+++ src/main/Restaurant.java
@@ -1,11 +1,22 @@
 package main;
 
+import display.components.CartListPanel;
 import display.views.RegisterLogin;
+
+import javax.swing.*;
 
 public class Restaurant
 {
 	public static void main(String[] args)
 	{
-		RegisterLogin rl = new RegisterLogin();
+		//RegisterLogin rl = new RegisterLogin();
+		
+		// REMOVE LATER, FOR TESTING
+		JFrame frame = new JFrame();
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.add(new CartListPanel("Example Title", '\u20AC', 2));
+		frame.setSize(400, 500);
+		frame.setLocationRelativeTo(null); // Center
+		frame.setVisible(true);
 	}
 }
