78d4844c159161bebb3729d44f55b0b7f771863e
conor96 <conormoloney57@gmail.com>
2018-11-16 00:13:08 +0000

    Implemented update and delete functions.
    Still need to refactor select to return String[][]

--- src/data/SQLConnector.java
+++ src/data/SQLConnector.java
@@ -2,8 +2,11 @@ package data;
 
 import java.sql.Connection;
 import java.sql.*;
+import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 
 public class SQLConnector implements DatabaseConnector
@@ -11,58 +14,147 @@ public class SQLConnector implements DatabaseConnector
 	Connection conn = null;
     private Statement statement = null;
     private PreparedStatement preparedStatement = null;
-    private ResultSet resultSet = null;
+    private ResultSet rs = null;
+
 	
 	@Override
-	public Connection getConnection() throws Exception
+	public void getConnection() throws Exception
 	{
-		try {
-            Class.forName("com.mysql.jdbc.Driver");
-            conn = DriverManager.getConnection("jdbc:mysql://localhost/accounts?"+ "user=sqluser&password=sqluserpw");
+		try{
+        Class.forName("com.mysql.jdbc.Driver");
+        conn = DriverManager.getConnection("jdbc:mysql://localhost/accounts?"+ "user=sqluser&password=sqluserpw");
 
-            return conn;
-        } catch(ClassNotFoundException exc) {
+        //return conn;
+       }
+       catch(ClassNotFoundException exc){
             System.out.println(exc);
             exc.printStackTrace();
             throw exc;
            
            //System.out.println("Could not find data");
-        } catch(SQLException exc) {
-		    exc.printStackTrace();
-            throw exc;
-        } finally {
-
-        }
+       }
+       
+      catch(SQLException exc){
+          exc.printStackTrace();
+          throw exc;
+      }
+       finally{
+               
+       }
+		
 	}
 
-    public ResultSet select(String table, String[] columns, String whereClause, String orderByClause)
-    {
+    public ResultSet select(String table, String[] columns, String whereClause, String orderByClause) {
         String result = "";
+        String strQuery = "Select";
+        for(int i=0;i<columns.length;i++){
+            strQuery+= " " + columns[i];
+            if(i+1<columns.length){
+                strQuery+=", ";
+            }
+        }
+        strQuery += " FROM $tablename";
+        if(whereClause!=null){
+            strQuery+=whereClause;
+        }
+        if(orderByClause!=null){
+            strQuery+=orderByClause;
+        }
+        String sql =strQuery.replace("$tablename",table);
+        System.out.println(sql);
         try {
-            statement = conn.createStatement();
-            //resultSet = statement.executeQuery(SQLCmd);
+            preparedStatement =conn.prepareStatement(sql);
+
+            System.out.println(preparedStatement);
+            rs = preparedStatement.executeQuery();
+            ResultSetMetaData rsmd = rs.getMetaData();
+
+            int numberColumns = rsmd.getColumnCount();
+            String [] row = new String[numberColumns];
             //result = writeResultSet(resultSet);
+            ArrayList allRows = new ArrayList();
+            while(rs.next()){
+                //System.out.println(rs);
+                String[] currentRow = new String[numberColumns];
+                for(int i = 1;i<=numberColumns;i++){
+                    row[i-1]=rs.getString(columns[i-1]);
+                    System.out.println(rs.getString(columns[i-1]) );
+                }
+                allRows.add(row);
+                String [][] arrayRows = new String[allRows.size()][numberColumns];
+                for(int i=0;i<allRows.size();i++){
+                    String [] r = (String[])allRows.get(i);
+                    for(int j=0;j<r.length;j++){
+                        
+                    }
+                }
+                /*
+                for(int i=0;i<allRows.size();i++){
+                     String [] r = (String [])allRows.get(i);
+                     arrayRows[i] = r;
+                     //arrayRows[i] = allRows.get(i);
+                }
+                //arrayRows = allRows.toString(arrayRows);
+                for(int i=0;i<arrayRows.length;i++){
+                    for(int j=0;i<arrayRows[i].length;j++){
+                        System.out.print(arrayRows[i][j] );
+                    }
+                    System.out.println();
+                }
+                */
+                //System.out.println(allRows);
+                //String [][] rowsFound = Arrays.toString(allRows.get(i));
+            }
+            //System.out.println(allRows);
         } catch(Exception e) {
             logError(e);
+
         }
-        return resultSet;
+        return rs;
         //return result;
     }
 
-    public boolean update(String table, String[] columns, String[] values, String whereClause)
-    {
+    public boolean update(String table, String[] columns, String[] values, String whereClause) {
+        String strQuery = "Update $tablename SET";
+        for(int i =0;i<columns.length;i++){
+              String set = " "+ columns[i] + "=" + "'" + values[i] + "'";
+              strQuery +=set;
+            if(i+1<columns.length){
+              strQuery+=", ";
+            }
+        }
+        if(strQuery!=null) {
+            strQuery += " " + whereClause;
+        }
+        String sql =strQuery.replace("$tablename",table);
+        System.out.println(sql);
+        int registerSuccess = 0;
         try {
-           // preparedStatement = conn.prepareStatement(SQLCmd);
-           // int rows = preparedStatement.executeUpdate();
-            return true;
+            preparedStatement = conn.prepareStatement(sql);
+            //preparedStatement.setString(1, table);
+            /*
+            for(int i=0;i<values.length;i++) {
+                preparedStatement.setString(i+1, values[i]);
+
+            }
+            */
+            System.out.println(preparedStatement);
+            registerSuccess = preparedStatement.executeUpdate();
+            if(registerSuccess>0){
+                return true;
+            }
+            else{
+                return false;
+            }
+
+
         } catch(Exception e) {
             logError(e);
             return false;
         }
     }
 
-    public boolean insert(String table, String[] columns, String[] values)
-    {
+    public boolean insert(String table, String[] columns, String[] values) {
         String strQuery = "INSERT INTO $tablename values(default,?,?) ";
         String sql =strQuery.replace("$tablename",table);
         System.out.println(sql);
@@ -72,14 +164,18 @@ public class SQLConnector implements DatabaseConnector
             //preparedStatement.setString(1, table);
             for(int i=0;i<values.length;i++) {
                 preparedStatement.setString(i+1, values[i]);
+
             }
             System.out.println(preparedStatement);
             registerSuccess = preparedStatement.executeUpdate();
-            if(registerSuccess>0) {
+            if(registerSuccess>0){
                 return true;
-            } else {
+            }
+            else{
                 return false;
             }
+
+
         } catch(Exception e) {
             logError(e);
             return false;
@@ -87,22 +183,46 @@ public class SQLConnector implements DatabaseConnector
         //return registerSuccess;
     }
 
-    public boolean delete(String table, String whereClause)
-    {
+    public boolean delete(String table, String whereClause) {
+        String strQuery = "DELETE FROM $tablename";
+        String sql =strQuery.replace("$tablename",table);
+        sql += " " + whereClause
+;        System.out.println(sql);
+        int deleteSuccess = 0;
         try {
-            //preparedStatement = conn.prepareStatement(SQLCmd);
-            //int rows = preparedStatement.executeUpdate();
-            return true;
+
+            preparedStatement = conn.prepareStatement(sql);
+
+
+
+
+            System.out.println(preparedStatement);
+            deleteSuccess = preparedStatement.executeUpdate();
+            if(deleteSuccess>0){
+                return true;
+            }
+            else{
+                return false;
+            }
+
         } catch(Exception e) {
             logError(e);
             return false;
         }
+
     }
 
-    private void logError(Exception e)
-    {
+    private void logError(Exception e){
         Logger.getLogger(SQLConnector.class.getName()).log(Level.SEVERE, null, e);
     }
+
+    public void closeConnection() throws Exception{
+	    conn.close();
+    }
+
+
 }
+
+
 
 
