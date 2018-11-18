43831216ae76c83e7485b481bddc24726afaac70
conor96 <conormoloney57@gmail.com>
2018-11-18 00:41:30 +0000

    Updated register controller to handle additional registration fields

--- src/controllers/RegisterController.java
+++ src/controllers/RegisterController.java
@@ -2,41 +2,43 @@ package controllers;
 
 import display.views.RegisterView;
 import data.RegisterModel;
+import display.views.LoginView;
 
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-public class RegisterController
-{
+public class RegisterController {
     RegisterModel model;
     RegisterView view;
     boolean registered;
     boolean alreadyRegistered = true;
 
-    public RegisterController(RegisterView view) throws Exception
-    {
-          model = new RegisterModel();
-          this.view = view;
+    public RegisterController(RegisterView view) throws Exception{
+        model = new RegisterModel();
+        this.view = view;
     }
 
-    public void checkRegistration(String user, char[] pass) throws Exception
-    {
+    public void checkRegistration(String name, String surname, String user, char[] pass, String email, String address, String phone) throws Exception{
         try {
             alreadyRegistered = model.checkRegistered(user, pass);
             if (!alreadyRegistered) {
                 //User is not already registered, so register them
-                model.registerUser(user, pass);
+                model.registerUser(name, surname, user, pass, email, address, phone);
                 registered = model.getRegistered();
                 if (registered) {
                     view.setMessage("Successfuly registered");
+                    LoginView login = new LoginView();
+
                 } else {
                     view.setMessage("Error registering");
                 }
             } else {
                 view.setMessage("User already exists");
             }
-        } catch(Exception exc) {
+        }
+        catch(Exception exc){
             Logger.getLogger(RegisterController.class.getName()).log(Level.SEVERE, null, exc);
         }
     }
 }
+
