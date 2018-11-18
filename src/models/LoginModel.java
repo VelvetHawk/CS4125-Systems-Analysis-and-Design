c91c325d65ed74637bb40b06f9f4c0e804373382
conor96 <conormoloney57@gmail.com>
2018-11-18 00:36:02 +0000

    Updated model to handle Where clause

--- src/models/LoginModel.java
+++ src/models/LoginModel.java
@@ -15,38 +15,43 @@ import data.SQLConnector;
  *
  * @author conor_000
  */
-public class LoginModel
-{
+public class LoginModel {
+
     Connection conn = null;
     private boolean loginValid = false;
 
-    public boolean getLogin()
-    {
-       return loginValid;
+    public boolean getLogin(){
+        return loginValid;
     }
 
-    public void loginValidation(String user, char [] passChars ) throws Exception
-    {
-        if(passChars!=null)
-        {
+    public void loginValidation(String user, char [] passChars ) throws Exception{
+
+        if(passChars!=null) {
             String pass=new String(passChars);
+            //add '' to values to allow them to be added to sql queries
+            user = "'" + user  + "'";
+            pass = "'" + pass + "'";
             SQLConnector SQLconn = new SQLConnector();
-            conn = SQLconn.getConnection();
-            String sql="SELECT username, password FROM TakeawayAccounts where username=? and password=?";
-            PreparedStatement ps=conn.prepareStatement(sql);
-            ps.setString(1,user);
-            ps.setString(2,pass);
-            ResultSet rs=ps.executeQuery();
+            SQLconn.getConnection();
+            String [] columns = {"username", "password"};
+            ResultSet rs = SQLconn.select("users", columns, " WHERE username =" + user + " AND PASSWORD =" + pass, null );
             if(rs.next()) {
-               //found
-               loginValid = true;
-            } else {
-               //not found
-               loginValid = false;
+                //found
+
+                loginValid = true;
+
             }
+            else{
+                //not found
+
+                loginValid = false;
+
+            }
+
+            System.out.println(loginValid);
             rs.close();
-            ps.close();
-            conn.close();
+            SQLconn.closeConnection();
+
         }
     }
 }
