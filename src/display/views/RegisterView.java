7bb9acab5b34e5150f71f86953b40949f4e24a09
conor96 <conormoloney57@gmail.com>
2018-11-18 00:40:20 +0000

    Added fields to registration form

--- src/display/views/RegisterView.java
+++ src/display/views/RegisterView.java
@@ -13,6 +13,16 @@ public class RegisterView extends JFrame {
     private JLabel passwordLabel;
     private JTextField nameText;
     private JPasswordField passwordField;
+    private JLabel surnameLabel;
+    private JTextField surnameText;
+    private JLabel usernameLabel;
+    private JTextField usernameText;
+    private JLabel emailLabel;
+    private JTextField emailText;
+    private JLabel addressLabel;
+    private JTextField addressText;
+    private JLabel phoneLabel;
+    private JTextField phoneText;
     private JButton submitButton;
     private JLabel errorLabel;
     private JLabel msg;
@@ -25,11 +35,24 @@ public class RegisterView extends JFrame {
         setSize(178, 190);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
 
-        nameLabel = new JLabel("User ID: ");
+
+        nameLabel = new JLabel("Name: ");
         add(nameLabel);
 
         nameText = new JTextField(10);
         add(nameText);
+
+        surnameLabel = new JLabel("Surname: ");
+        add(surnameLabel);
+
+        surnameText = new JTextField(10);
+        add(surnameText);
+
+        usernameLabel = new JLabel("Username: ");
+        add(usernameLabel);
+
+        usernameText = new JTextField(10);
+        add(usernameText);
 
         passwordLabel = new JLabel("Password: ");
         add(passwordLabel);
@@ -37,9 +60,26 @@ public class RegisterView extends JFrame {
         passwordField = new JPasswordField(10);
         add(passwordField);
 
+        emailLabel = new JLabel("Email: ");
+        add(emailLabel);
+
+        emailText = new JTextField(10);
+        add(emailText);
+
+        addressLabel = new JLabel("Address: ");
+        add(addressLabel);
+
+        addressText = new JTextField(20);
+        add(addressText);
+
+        phoneLabel = new JLabel("Phone: ");
+        add(phoneLabel);
+
+        phoneText = new JTextField(20);
+        add(phoneText);
+
         submitButton = new JButton("Submit");
         add(submitButton);
-
 
         RegisterController controller = new RegisterController(this);
 
@@ -47,19 +87,24 @@ public class RegisterView extends JFrame {
 
 
         submitButton.addActionListener(new ActionListener() {
-           public void actionPerformed(ActionEvent e) {
-               try {
-                   String user = nameText.getText();
-                   char[] passChars = passwordField.getPassword();
-                   controller.checkRegistration(user, passChars);
-               }
-               catch(Exception exc){
+            public void actionPerformed(ActionEvent e) {
+                try {
+                    String name = nameText.getText();
+                    String surname = surnameText.getText();
+                    String user = usernameText.getText();
+                    char[] passChars = passwordField.getPassword();
+                    String email = emailText.getText();
+                    String address = addressText.getText();
+                    String phone = phoneText.getText();
+                    controller.checkRegistration(name, surname, user, passChars, email, address, phone);
+                }
+                catch(Exception exc){
 
-               }
+                }
 
 
-           }
-       });
+            }
+        });
     }
 
     public void setMessage(String message){
@@ -68,3 +113,4 @@ public class RegisterView extends JFrame {
         add(msg);
     }
 }
+
