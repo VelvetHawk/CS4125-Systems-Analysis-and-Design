4e195d1a1bbdd311e4e9de8d24bab2f11a470804
conor96 <conormoloney57@gmail.com>
2018-11-18 00:44:25 +0000

    Updated model to handle new fields and connect to to actual database rather than demo

--- src/data/RegisterModel.java
+++ src/data/RegisterModel.java
@@ -1,57 +1,56 @@
 package data;
 
-import java.sql.*;
 import java.sql.Connection;
 
-import data.SQLConnector;
 
+public class RegisterModel{
 
-public class RegisterModel
-{
     Connection conn = null;
     boolean alreadyRegistered = false;
     boolean registered = false;
     SQLConnector SQLconn = new SQLConnector();
-    //SQLConnector sqlConn = new SQLConnector;
 
-    public RegisterModel() throws Exception
-    {
-        conn = SQLconn.getConnection();
+    public RegisterModel() throws Exception {
+
+        SQLconn.getConnection();
     }
 
-    public boolean checkRegistered(String user, char[] pass)
-    {
-         return alreadyRegistered;
+    public boolean checkRegistered(String user, char[] pass){
+        //TODO: check if registered
+        return alreadyRegistered;
     }
 
-    public boolean getRegistered()
-    {
+    public boolean getRegistered(){
         return registered;
     }
 
-    public void registerUser(String user, char[] pass) throws Exception
-    {
-         String password = new String(pass);
-         String [] columns = new String[2];
-         String [] values = new String[2];
-         values[0] = user;
-         values[1] = password;
-         //String sql = "INSERT INTO accounts.takeawayaccounts values(default,?,?) ";
-         boolean registerSuccess = SQLconn.insert("accounts.takeawayaccounts", columns, values);
-         //PreparedStatement ps = conn.insert(sql, user, pass);
-         //PreparedStatement ps = conn.prepareStatement(sql);
-        /*
-         ps.setString(1, user);
-         ps.setString(2, password);
-         int registerSuccess = ps.executeUpdate();
-         */
-         if(registerSuccess) {
-             registered = true;
-         } else {
-             registered = false;
-         }
-            //ps.close();
-            conn.close();
+    public void registerUser(String name, String surname, String user, char[] pass, String email, String address, String phone) throws Exception{
+        String password = new String(pass);
+        String [] columns = new String[12];
+        String [] values = new String[12];
+        values[0] = "default";
+        values[1] = name;
+        values[2] = surname;
+        values[3] = user;
+        values[4] = password;
+        values[5] = email;
+        values[6] = address;
+        values[7] = phone;
+        values[8] = "default";
+        values[9] = "default";
+        values[10] = "default";
+        values[11] = "default";
+        boolean registerSuccess = SQLconn.insert("users", columns, values);
+        if(registerSuccess){
+            registered = true;
+        }
+        else{
+            registered = false;
+        }
+        
+        SQLconn.closeConnection();
     }
 }
+
+
 
