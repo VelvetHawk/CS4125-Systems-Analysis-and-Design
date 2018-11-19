32ab4296b0b59b1927cdf429af16e9d00ab5f013
samemaan <fahim.emaan@hotmail.com>
2018-11-19 13:11:47 +0000

    pattern states

---
+++ src/consumables/states/ReceivedOrderState.java
@@ -0,0 +1,14 @@
+package consumables.states;
+
+public class ReceivedOrderState implements State {
+
+    public void doAction(Orders orders) {
+        System.out.println("Order is received");
+        orders.setState(this);
+    }
+
+    public String toString(){
+
+        return "Order received State";
+    }
+}
