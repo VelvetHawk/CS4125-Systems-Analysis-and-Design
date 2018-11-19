1450d2fe45fc0fd61617ba70936c045ce7f848c2
samemaan <fahim.emaan@hotmail.com>
2018-11-19 13:11:07 +0000

    pattern states

---
+++ src/consumables/states/Orders.java
@@ -0,0 +1,20 @@
+package consumables.states;
+
+public class Orders {
+   private State state;
+
+   public Orders(){
+
+      state = null;
+   }
+
+   public void setState(State state){
+
+      this.state = state;
+   }
+
+   public State getState(){
+
+      return state;
+   }
+}
