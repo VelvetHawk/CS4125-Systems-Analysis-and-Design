cac7b59f75a4200551976ca5b8e7c8bda9a8baba
samemaan <fahim.emaan@hotmail.com>
2018-11-19 13:09:27 +0000

    pattern states

---
+++ src/consumables/states/CancelOrderState.java
@@ -0,0 +1,13 @@
+package consumables.states;
+
+public class CancelOrderState implements State {
+    public void doAction(Orders orders) {
+        System.out.println("Order is in cancel state");
+        orders.setState(this);
+    }
+
+    public String toString(){
+
+        return "Order cancel State";
+    }
+}
