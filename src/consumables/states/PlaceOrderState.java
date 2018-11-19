dcf74ba5609f8252b9242f1ca727b11b4905c0e0
samemaan <fahim.emaan@hotmail.com>
2018-11-19 13:08:27 +0000

    pattern states

---
+++ src/consumables/states/PlaceOrderState.java
@@ -0,0 +1,14 @@
+package consumables.states;
+
+public class PlaceOrderState implements State {
+
+    public void doAction(Orders orders) {
+        System.out.println("Order is in Progress state");
+        orders.setState(this);
+    }
+
+    public String toString(){
+
+        return "Progress State";
+    }
+}
