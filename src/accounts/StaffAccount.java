79e5482aa3c4e9c271e7e16dc0ab5b7d2ae2e646
BrianED <brian_dooley9@yahoo.ie>
2018-12-01 15:36:33 +0000

    Added getters and setters for Account types

--- src/accounts/StaffAccount.java
+++ src/accounts/StaffAccount.java
@@ -4,4 +4,24 @@ public class StaffAccount extends Account
 {
 	protected int staffNumber;
 	protected Employee employeeType;
+
+	public int getStaffNumber()
+	{
+		return staffNumber;
+	}
+
+	public void setStaffNumber(int staffNumber)
+	{
+		this.staffNumber = staffNumber;
+	}
+
+	public Employee getEmployeeType()
+	{
+		return employeeType;
+	}
+
+	public void setEmployeeType(Employee employeeType)
+	{
+		this.employeeType = employeeType;
+	}
 }
